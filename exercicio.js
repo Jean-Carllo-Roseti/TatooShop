c67b08ee9b094f5df5934dcb7dae7ac615bd08b6
jean carllo <jeanrosete04@gmail.com>
2024-01-04 14:44:20 -0300

    finalizada a atividade.

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplica = (a, b) => a * b;
+function saudacao(nome) {
+    return (" Ola" + nome);
+}
+console.log(multiplica(3, 5));
+console.log(saudacao("Jurema"));
--- exercicio.ts
+++ exercicio.ts
@@ -1,9 +1,9 @@
+const multiplica = (a: number, b:number): number =>  a * b ;
 
 
+function saudacao (nome: string): string{
+    return (" Ola" + nome);
+}
 
-
-
-
-
-
-
+console.log(multiplica(3, 5));
+console.log(saudacao("Jurema"));
